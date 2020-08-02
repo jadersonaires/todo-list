43cad7bee0c59d5967112f91354817a83d7110f1
Jaderson Aires <jadersonaires@gmail.com>
2020-08-02 16:58:06 -0300

    Updates

---
+++ precache-manifest.e12f50a3c7d3e461a112445e2834a15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cba5cf68e5f81536e6d2b193f745da0",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d56fba4416ac87070cbf",
+    "url": "/todo-list/static/css/main.fc3226f5.chunk.css"
+  },
+  {
+    "revision": "b2d7f68d7caa1f05e1fb",
+    "url": "/todo-list/static/js/2.d5e68eec.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/todo-list/static/js/2.d5e68eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56fba4416ac87070cbf",
+    "url": "/todo-list/static/js/main.761755fe.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
